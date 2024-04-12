b30ef319d6d833dd9ac98711b2feb2ac85b8be51
JoHyunwoo <jhw3874@gmail.com>
2024-04-12 17:22:18 +0900

    README.md 요구사항 작성 및 .gitignore 설정

---
+++ src/main/java/com/asap/openrun/OpenRunApplication.java
@@ -0,0 +1,13 @@
+package com.asap.openrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asap/openrun/OpenRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asap.openrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
